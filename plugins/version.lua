d098f76e83bc2531da92540565506557b6626810
siilaas <fire_matter@yahoo.com>
2016-04-16 17:50:24 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -14,7 +14,7 @@
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot\n\nDevelopers: @MaSkAoS @Josepdal\nSupport: @skneos @iicc1 @Serx666\nChannels: @DBTeamEN @DBTeamES\n\nCheckout: https://github.com/Josepdal/DBTeam\nGNU GPL v2 license.'
+  return 'gto-bot V3 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot\n\nDevelopers: @oralius @mmaahhssaa1\nSupport: @skneos @iicc1 @Serx666\nChannels: @gto-botEN @oraliusES\n\nCheckout: https://github.com/siilaas/gto-bot\nGNU GPL v2 license.'
 end
 
 return {
